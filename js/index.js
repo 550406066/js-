79ba910c595c7c7a861fdd346f48e606c80acc9c
DESKTOP-7ANOCBD\M80 <550406066@qq.com>
2018-06-04 13:27:18 +0800

    完成功能

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
 var pullDown=function(){
+    this.html='<div class="main">\
+        <input type="text"  id="makeEnter" class="makeEnter" placeholder="请选择或输入"/>\
+        <ul  id="optionList" class="optionList">\
+        </ul>\
+        <div  id="icon" class="icon iconfont icon-xiala pull-down-icon"></div>\
+    </div>'
     this.TempArr = [];
     this.ajaxUrl={
         demoUrl:"https://easy-mock.com/mock/5b0d5a9d2179ff1c604e3bf3/scroll_copy_copy/"
@@ -39,7 +45,10 @@ pullDown.prototype={
         var TempArr=this.TempArr;
         var makeEnterVal=$("#makeEnter").val()
         select.html("");
-   
+        console.time('setinput'); // 启动计时, 该时刻命名为 setinput (可以自定)
+        // ...  要执行的代码块
+     
+     
         for (i = 0; i < TempArr.length; i++) {
         
             //若找到以txt的内容开头的，添option  
@@ -53,33 +62,45 @@ pullDown.prototype={
                 // var option = $("<li></li>").html("<em>"+TempArr[i]+"</em>");  
                 // var option = $("<li></li>").text(TempArr[i]);  
                 select.append(option);
+                console.timeEnd('setinput'); // 定时结束, 浏览器控制台将输出代码块的执行时间
             }
+        }
+        if( $("#optionList").html()===""){
+            select.append("<span class='no-data'>未找到数据···</span>")
         }
     },
     bind:function(){
         var _this=this
+ 
         $("#optionList li").each(function (index, el) {
             _this.TempArr[index] = $(this).text();
         });
-        $("#makeEnter").bind("focus",function(){
+        $("body").off("click","#makeEnter").on("click","#makeEnter",function(){
+  
             _this.setfocus()
         })
-        $("#icon").bind('click', function (e) {
-            $("#optionList").css({ "display": "block" });
+        $("body").off("click","#icon").on("click","#icon",function(){
+            if(  $("#optionList").css("display")=="block"){
+                $("#optionList").css({ "display": "none" })
+            }else{
+                $("#optionList").css({ "display": "block" });
+            }
+           
         })
-        $("#optionList li").bind('click',function () {
+        $("body").off("click","#optionList li").on("click","#optionList li",function(){
      
             $(this).parent().prev("#makeEnter").val($(this).text());
             $("#optionList").css({ "display": "none" });
         });
-        $("#makeEnter").bind("input",function(){
+        $("body").off("input","#makeEnter").on("input","#makeEnter",function(){
+    
             _this.setinput()
         })
   
     },
     init:function(){
         this.getData();
-      
+      $("body").append(this.html)
     }
 }
 var pullDown=new pullDown()
